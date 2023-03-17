869ed11618b68dd6c11e68a33e7d4f03671f1bd4
Ryan <r.alcoran@gmail.com>
2023-03-17 00:12:21 -0400

    Added boilerplate html, created script and added basic math operators: add, subtract, multilpy, divide

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function add(a, b){
+	return a + b;
+}
+
+function subtract(a, b){
+	return a - b;
+}
+
+function multiply(a, b){
+	return a * b;
+}
+
+function divide(a, b){
+	return a / b;
+}
+
+console.log(`4 + 3 = ${add(4, 3)}`);
+console.log(`8 - 12 = ${subtract(8, 12)}`);
+console.log(`6 * 9 = ${multiply(6, 9)}`);
+console.log(`15 / 3 = ${divide(15, 3)}`);
