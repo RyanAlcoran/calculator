d04fc751b91c64064c5ab0528ef02b524f3868a4
Ryan <r.alcoran@gmail.com>
2023-03-17 00:20:50 -0400

    Added operate function hat takes in operator and 2 numbers then calls the one of the operators on the twp numbers

--- js/script.js
+++ js/script.js
@@ -14,7 +14,13 @@ function divide(a, b){
 	return a / b;
 }
 
+function operate(func, a, b){
+	return func(a, b);
+}
+
 console.log(`4 + 3 = ${add(4, 3)}`);
 console.log(`8 - 12 = ${subtract(8, 12)}`);
 console.log(`6 * 9 = ${multiply(6, 9)}`);
 console.log(`15 / 3 = ${divide(15, 3)}`);
+
+console.log(`6 + 6 = ${operate(add, 6, 6)}`);
