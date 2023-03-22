31612544c3e71e4eed6897930696945fdbe53ab6
Ryan <r.alcoran@gmail.com>
2023-03-21 21:05:03 -0400

    Added backspace icon

--- js/script.js
+++ js/script.js
@@ -1,13 +1,77 @@
-let display = '';
-const results = document.querySelector(".results");
-const btns = document.querySelectorAll(".btn");
+let input = '';
+let display = ''
+let firstOperand = '';
+let secondOperand = '';
+let operator = '';
+
+const results = document.querySelector('.results');
+
+const btns = document.querySelectorAll('.btn');
 btns.forEach((btn) => {
-	btn.addEventListener("click", () => {
-	console.log(btn.getAttribute("data-value"));
-	display += btn.getAttribute("data-value");
-	results.textContent = display;
+	btn.addEventListener('click', () => {
+		//console.log(btn.getAttribute('data-type'));
+		switch (btn.getAttribute('data-type')) {
+		case 'clear':
+			clear();
+			break;
+		case 'backspace':
+			backspace(input);
+			break;
+		case 'num':
+			//if (input.length < 9) {
+			input += btn.textContent;
+			results.textContent = input;
+			//}
+			break;
+		case 'operator':
+			firstOperand = input;
+			input += btn.textContent;
+			results.textContent = input
+			break;
+		case 'operate':
+			break;
+		default:
+			console.log("does not match");
+		}
+		
 	});
 });
+// Buttons
+// const btnsNum = document.querySelectorAll('.btn.num');
+// btnsNum.forEach((btn) => {
+// 	btn.addEventListener('click', () => {
+// 		if (input.length < 9) {
+// 			input += btn.textContent;
+// 			results.textContent = input;
+// 		}
+// 	});
+// });
+
+// const btnClear = document.querySelector('[data-value="clear"]');
+// btnClear.addEventListener('click', clear);
+
+// const
+
+// const btnsOp = document.querySelectorAll('.btn.op');
+// btnsOp.forEach((btn) => {
+// 	btn.addEventListener('click', () => {
+// 		operator = btn.getAttribute('data-value');
+// 		input += ` ${btn.textContent} `;
+// 	});
+// });
+
+function backspace(string) {
+	input = string.slice(0, -1);
+	results.textContent = input;
+}
+
+function clear() {
+	input = '';
+	firstOperand = '';
+	secondOperand = '';
+	operator = '';
+	results.textContent = '0';
+}
 
 function add(a, b){
 	return a + b;
